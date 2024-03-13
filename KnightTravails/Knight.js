ac96f5d0410b7132f79b5f5dbaf0147fde848cf8
rayobiel <gabrielfandradee2@gmail.com>
2024-03-13 19:12:52 -0300

    I'll remake it one day

--- KnightTravails/Knight.js
+++ KnightTravails/Knight.js
@@ -1,36 +1,33 @@
-class createRoot {
+class Node {
     constructor(position) {
-        // this.position = position;
-        this.topLeftSide = null;
-        this.topLeftTop = null;
-        this.topRightSide = null;
-        this.topRightTop = null;
-        this.lowLeftSide = null;
-        this.lowLeftTop = null;
-        this.lowRightSide = null;
-        this.lowRightTop = null;
-        this.right = null;
+        this.position = position;
+        this.edges = [];
     }
 }
 
 class KnightTravails {
-    constructor(position) {
-        this.board = () => {
-            const squareBoard = [];
-            for (let i = 0; i < 8; i++) {
-                for (let j = 0; j < 8; j++) {
-                    squareBoard.push([i, j]);
-                }
-            } 
-           return squareBoard;
-        }  
+    constructor() {
+        this.board = this.createBoard();
+        this.graph();
+    }
+
+    createBoard() {
+        const squareBoard = [];
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                const newSquare = new Node([i, j])
+                squareBoard.push(newSquare);
+            }
+        } 
+        return squareBoard;
     }
 
     graph() {
-        const adjList = []
-        this.board().forEach(e => {
+        const squares = this.board;
+        let adjList = [];
+        squares.forEach(e => {
             const edges = [];
-            const [row, column] = e;
+            const [row, column] = e.position;
 
             const moves = [
                 [row + 1, column - 2],
@@ -45,25 +42,89 @@ class KnightTravails {
 
             moves.forEach(([newRow, newColumn]) => {
                 if (newRow >= 0 && newRow < 8 && newColumn >= 0 && newColumn < 8) {
-                    edges.push([newRow, newColumn]);
+                    const newIndex = this.findIndex([newRow, newColumn]);
+                    edges.push(newIndex);
                 }
             });
-
-            adjList.push(edges);
+            adjList.push(edges)
         })
-        return adjList;
+        return adjList
     }
 
-
-    KnightMove(position) {
-        const newNode = new createRoot(position)
+    findIndex(value) {
+        const graph = this.board;
+        for (let i = 0; i < graph.length; i++) {
+            const nodePosition = graph[i].position;
+            if (nodePosition[0] === value[0] && nodePosition[1] === value[1]) {
+                return i;
+            }
+        }
+        return null;
     }
 
-    LowerRoute() {
+    buildInfoArr(boardArr, startIndex) {
+        let newArr = [];
+        for (let i = 0; i < boardArr.length; i++) {
+            newArr[i] = {
+                distance: null,
+                predecessor: null,
+            }
+        }
+        newArr[startIndex].distance = 0;
+        console.log(newArr);
+        return newArr;
+    }
+
+    constructPath(board, infoArr, item, index, newArr) {
+        if (item.predecessor === null) return;
+        if (item.predecessor != null) {
+            newArr.push(board[index]);
+            this.constructPath(board, infoArr, infoArr[item.predecessor], item.predecessor, newArr);
+        }
+    }
+
+    KnightMoves(start, end) {
+
+        let board = this.board;
+        const startIndex = this.findIndex(start);
+        const endIndex = this.findIndex(end);
+        let bfsInfo = this.buildInfoArr(board, startIndex);
+        let adjList = this.graph();
+        let queue = [startIndex];
+        let u;
+
+        while(u != endIndex) {
+            u = queue.shift();
+
+            
+            for (let i = 0; i < adjList[u].length; i++) {
+                let vIndex = adjList[u][i];
+                console.log(vIndex)
+                if (vIndex === endIndex) {
+                    bfsInfo[vIndex].predecessor = u;
+                    let path = [];
+                    this.constructPath(board, bfsInfo, bfsInfo[vIndex], vIndex, path);
+                    let result = path.reverse().splice(0,0, start);
+                    console.log(`You made it in ${path.length - 1} move! Here's your path: ${result}`);
+                    return path
+                } else {
+                    if (bfsInfo[vIndex].distance === null) {
+                        bfsInfo[vIndex].distance = bfsInfo[u].distance + 1;
+                        bfsInfo[vIndex].predecessor = u;
+                        queue.push(vIndex);
+                    }
+                }
+            }
+        }
     }
 }
 
+// Da próxima vez, liste as arestas por seu index
+// Entenda o conceito de distance e predecessor
+
 const teste = new KnightTravails;
 
-console.log(teste.graph());
+console.log(teste.findIndex([0, 0]));
+
+
 
